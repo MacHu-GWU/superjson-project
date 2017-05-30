15bf27ea946db12c8379056b059be20dd0bb29d0
Sanhe <husanhe@gmail.com>
2017-05-30 17:18:46 -0400

    2017-05-30

--- superjson/__init__.py
+++ superjson/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-__version__ = "0.0.2"
+__version__ = "0.0.3"
 __short_description__ = "Extendable json encode/decode library."
 __license__ = "MIT"
 
